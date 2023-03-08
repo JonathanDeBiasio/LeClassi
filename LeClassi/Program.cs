18145d6428243456e0a6caf7d552f3e4a0e7562c
Jonathan <Jonathan@CLAUDIO-PC>
2023-03-08 12:41:41 +0100

    Aggiungere i file di progetto.

---
+++ LeClassi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeClassi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Jonathan",23);
+            Person.fondoComune = 1000.00M;
+            Person person2 = new Person("Carlo", 30);
+            person2.GetName();
+            Console.WriteLine(Person.fondoComune);
+            Console.WriteLine("adding more value....");
+            Person.fondoComune += 1000.00M;
+            
+            Console.WriteLine(Person.getFondoComune());
+        }
+    }
+
+    internal class Person
+    {
+       public static decimal fondoComune;
+         string _name;
+         int _age;
+
+        public Person(string Name, int Age)//firma del costruttore
+        {
+            _name = Name;
+            _age = Age;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        
+        public static decimal getFondoComune() 
+        {
+        }
+    }
+}
